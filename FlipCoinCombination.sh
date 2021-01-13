0274d10174249b638eff0e19b000763c43ac3210
lavanya <lavanyace929@gmail.com>
2021-01-13 19:32:56 +0530

    Added:How many times heads and tails won

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,11 +1,22 @@
 #!/bin/bash -x
+#variables
+count1=0
+count2=0
 
+#Constants
+IS_HEADS=1
+IS_TAILS=0
+read -p "Enter a no time to loop run: " n
+for (( i=1; i<=n; i++ ))
+do 
 FlipCoin=$(( RANDOM%2 ))
-IsHeads=1
-
-if [ $FlipCoin -eq $IsHeads ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+	if [ $FlipCoin -eq $IS_HEADS ]
+	then
+		((count1++))
+	else
+		((count2++))
+	
+	fi
+done
+echo "Head has won $count1 times"
+echo "Tail has won $count2 times"
