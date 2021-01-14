c6942465e87af98b186b69af9ee4d7bd0f15f9c8
lavanya <lavanyace929@gmail.com>
2021-01-14 11:24:34 +0530

    Added:Heads or Tails won till diffrence is two

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,30 +1,39 @@
 #!/bin/bash -x
+
 #variables
 count1=0
 count2=0
 
 #Constants
 IS_HEADS=1
-IS_TAILS=0
+VALUE=21
+VALUE1=25
+
 read -p "Enter a no time to loop run: " n
 for (( i=1; i<=n; i++ ))
 do 
-FlipCoin=$(( RANDOM%2 ))
+  FlipCoin=$(( RANDOM%2 ))
 	if [ $FlipCoin -eq $IS_HEADS ]
 	then
 		((count1++))
 	else
 		((count2++))
-	
 	fi
-	if [[ $count1 == 21 || $count2 == 21 ]]
+
+	if [[ $count1 -gt $VALUE && $count2 -gt $VALUE ]]
 	then
-		break
+		 if [[ $count1 -gt $VALUE1 || $count2 -gt $VALUE1 ]]
+       then
+				break
+		 fi	 	
 	fi	
 done
-if [ $count1 == 21 ]
+
+if [ $count1 -gt $count2 ]
 then
 	echo "Head has won over tail with diffrence of: " $(($count1-$count2))
 else
    echo "Tail has won over head with diffrence of: " $(($count2-$count1))
 fi
+echo $count1
+echo $count2
